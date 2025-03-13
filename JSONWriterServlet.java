e2f1e989c8769b2b4f5d33c6cf80daa6cca65d25
Sarah J Watters <69488759+Mjecuse@users.noreply.github.com>
2025-03-12 23:32:51 -0400

    Update JSONWriterServlet.java

--- JSONWriterServlet.java
+++ JSONWriterServlet.java
@@ -1,31 +1,18 @@
 import java.io.IOException;
-
 import javax.servlet.ServletException;
-
 import javax.servlet.annotation.WebServlet;
-
 import javax.servlet.http.HttpServlet;
-
 import javax.servlet.http.HttpServletRequest;
-
 import javax.servlet.http.HttpServletResponse;
-
-
 public class JSONWriterServlet extends HttpServlet {
-	
-
 protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
      String data = request.getParameter("data");
-     
-
      JSONWriter.writeToJsonFile("output.json", data);
 }
 /**
-
  * Servlet implementation class JSONWriterServlet
  * Problem was needed to skip the nesting. revised the recursion powerpoint sent and fixed it.
-
  */
 public class JSONWriterServlet extends HttpServlet {
  private static final long serialVersionUID = 1L;
@@ -38,22 +25,14 @@ public class JSONWriterServlet extends HttpServlet {
 	 if(i <= 1) {
 		 return;
 	 }
-	 JSONWriter(i - 1);
-	 
-	 
+	 JSONWriter(i - 1);	 
  }
  /**
  * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
  */
  protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-
  // TODO Auto-generated method stub
-
-
  response.getWriter().append("Served at: ").append(request.getContextPath());
-
-
  }
 
 
