90cf791c3f970785de2ddc9af55e53e39315cadc
Sarah J Watters <69488759+Mjecuse@users.noreply.github.com>
2025-03-12 23:30:50 -0400

    Update JSONWriter.java

--- JSONWriter.java
+++ JSONWriter.java
@@ -1,26 +1,11 @@
-
 import java.io.FileWriter;
-
 import java.io.IOException;
-
 import org.json.simple.JSONObject;
-
-
-
- 
-
 public class JSONWriter {
-
 public static void writeToJsonFile(String filename, String data) {
-
      JSONObject json = new JSONObject();
-
      json.put("data", data);
-
- 
-
      try (FileWriter file = new FileWriter(filename)) {
-
          file.write(json.toJSONString());
 
          file.flush();
